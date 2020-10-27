23eb16b4ea79de2c32f2a6b6ba79af6fe6e5cf2a
sebas6612 <sebas6612@gmail.com>
2020-10-27 12:33:17 +1000

    karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,13 @@ process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function(config) {
     config.set({
         basePath: '',
-        browsers: ['ChromeHeadless'],
+        browsers: ['Chrome'],
+        customLaunchers: {
+          ChromeHeadlessCI: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox']
+          }
+        },
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
             require('karma-jasmine'),
